5ca22751af07a49b33e8119963ee86be6972f73a
Armaan Amoli <91266111+ArmaanAmoli@users.noreply.github.com>
2025-12-18 20:10:35 +0530

    Fixing Indexing Issues

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
-function render(){
+function render() {
     const listOfTask = document.querySelector(".list")
-    listOfTask.innerHTML=``
+    listOfTask.innerHTML = ``
     //This function will render evety task stored in the local storage
     let LT = localStorage.getItem("tasks")
     LT = JSON.parse(LT)
-    if(LT==null){
+    if (LT == null) {
         return
     }
     let HTMLcontent = ""
-    for(let i = 0 ; i<LT.length ; i++){
-        HTMLcontent+=`
+    for (let i = 0; i < LT.length; i++) {
+        HTMLcontent += `
         <div class="task">
-                    <input type="checkbox" id="${i}" name="Checkbox" ${LT[i][4]?'checked':''}>
-                    <h3>${LT[i][0]}</h3>
-                    <p>${LT[i][1]} - ${LT[i][2]}</p>
+                    <input type="checkbox" id="${LT[i][5]}" name="Checkbox" ${LT[i][4] ? 'checked' : ''}>
+                    <div class = "Task-content">
+                        <h3>${LT[i][0]}</h3>
+                        <p>${LT[i][1]} - ${LT[i][2]}</p>
+                    </div>
+                    <button class="delete-task-button" id="DELETE-${LT[i][5]}"><img src="delete.svg"></button>
         </div>`
     }
     listOfTask.innerHTML = HTMLcontent
 
     //After rendering we will we attaching event listeners
     const Checkboxes = document.querySelectorAll('input[type="checkbox"]')
-    Checkboxes.forEach(cb=>
-        cb.addEventListener('change',handleCheckboxChange)
+    Checkboxes.forEach(cb =>
+        cb.addEventListener('change', handleCheckboxChange)
     )
 }
 
 let addNewTaskButton;
-
 
 //we use DOMContentLoaded because we are waiting for the html content to entirely load so that Javascript can safely access the element
 document.addEventListener("DOMContentLoaded", () => {
@@ -38,18 +40,23 @@ document.addEventListener("DOMContentLoaded", () => {
 // const BodyHtmlElement = document.body
 let newTaskForm;
 let taskWidget;
-function createNewTask(){
+let NewTaskWindowCloseButton;
+function createNewTask() {
     if (newTaskForm) {
         newTaskForm.remove();
     }
-    
+
     newTaskForm = document.createElement('div')
     newTaskForm.classList.add("NewTaskForm")
     newTaskForm.id = 'newT'
-    newTaskForm.innerHTML+=`
+    newTaskForm.innerHTML += `
         <div class = "new-task-header">
             <h1>Add a new New Task</h1>
-            <button class="save-new-task">save</button>
+            <div class = "utility-buttons">
+                <button class="Close">x</button>
+                <button class="save-new-task">save</button>
+            </div>
+            
         </div>
         <h2>Task Name</h2>
         <input type="text" placeholder="Enter new task" class="newTaskName">
@@ -70,26 +77,30 @@ function createNewTask(){
     `
     document.body.appendChild(newTaskForm)
     const saveTaskButton = document.querySelector(".save-new-task")
-    saveTaskButton.addEventListener('click',saveNewTask)
-    
+    saveTaskButton.addEventListener('click', saveNewTask)
+    NewTaskWindowCloseButton = document.querySelector(".Close")
+    NewTaskWindowCloseButton.addEventListener("click", closeWindow)
 }
 
-// as the user click "save" button we want to add new html element in the list class
+function closeWindow() { newTaskForm.remove() }// Close the window if you 'X' button clicked
 
-function saveNewTask(){
+function saveNewTask() { // as the user click "save" button we want to add new html element in the list class
+
+    //We will be going to store task ID as timestamp-> int meiliseconds from Jan 1 1970 UTC bye using -> Date.now()
     const newTaskName = document.querySelector(".newTaskName").value
     const StartDate = document.querySelector("#StartDate").value
     const Deadline = document.querySelector("#Deadline").value
     const TaskDescription = document.querySelector("#comment").value
     const IsDone = false // Initiating the task as not done
+    const ID = Date.now()
 
-    taskWidget = [newTaskName , StartDate , Deadline , TaskDescription , IsDone]
-    if(taskWidget[0]!=="" && taskWidget[1]!==""){
+    taskWidget = [newTaskName, StartDate, Deadline, TaskDescription, IsDone, ID]
+    if (taskWidget[0] !== "" && taskWidget[1] !== "") {
         Local = localStorage.getItem("tasks")
-        if(Local == null){
-            localStorage.setItem("tasks",JSON.stringify([taskWidget]))
+        if (Local == null) {
+            localStorage.setItem("tasks", JSON.stringify([taskWidget]))
         }
-        else{
+        else {
             let LocalArray = JSON.parse(Local)
             LocalArray.push(taskWidget)
             localStorage.setItem("tasks", JSON.stringify(LocalArray))
@@ -100,16 +111,17 @@ function saveNewTask(){
 }
 
 //Creating a function to detect change in a checkbox
-function handleCheckboxChange(event){
+function handleCheckboxChange(event) {
     const ClickedCheckBox = event.target
-    let idOfCB = ClickedCheckBox.id
-    //Through the ID we will update the local storage as the ID is same as the index of the particular Task in local storage
+    let idOfCB = Number(ClickedCheckBox.id)
+    //Through the ID we will update the local storage as the ID is stored in the 5th index of each subarray
     let taskArray = JSON.parse(localStorage.getItem("tasks"))
-    taskArray[Number(idOfCB)][4] = !taskArray[Number(idOfCB)][4]  // If checked returns true
-    console.log(taskArray[Number(idOfCB)][4] + " " + Number(idOfCB))
-    localStorage.setItem("tasks" , JSON.stringify(taskArray))
+
+    for (let i = 0; i < taskArray.length; i++) {
+        if (taskArray[i][5] == idOfCB) {
+            taskArray[i][4] = !taskArray[i][4]
+            break
+        }
+    }
+    localStorage.setItem("tasks", JSON.stringify(taskArray))
 }
-
-
-
-
